92a8a53bab556bc70c0edd5f06a8eea26663c4fd
Alexander Fink <alex@bitahoy.com>
2022-06-28 09:37:06 +0200

    extend opensearch.py and bitahoycloud.py

--- soarflow/src/bitahoycloud.py
+++ soarflow/src/bitahoycloud.py
@@ -1,21 +1,49 @@
+from datetime import datetime
 import websockets
 import aiohttp
 import asyncio
 import json
 from opensearch import OpenSearchConnection
-import sys
+import os
 
 
 
 async def get_auth_token():
     async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
-        async with session.post("https://auth.bitahoy.cloud/login", json={"email": sys.argv["BITAHOY_USER"], "password": sys.argv["BITAHOY_PASS"]}) as resp:
+        async with session.post("https://auth.bitahoy.cloud/login", json={"email": os.environ["BITAHOY_USER"], "password": os.environ["BITAHOY_PASS"]}) as resp:
             return (await resp.json())["token"]
 
 
 async def poll_blocked_domains(token):
     conn = OpenSearchConnection('https://admin:admin@localhost:9200/')
-    await conn.create_index('contentblocking')
+    print(await conn.create_index('contentblocking', {
+        
+  "mappings": {
+    "properties": {
+      "domain": {
+        "type": "text"
+      },
+      "category": {
+        "type": "text"
+        },
+        "reason": {
+        "type": "text"
+        },
+        "source": {
+        "type": "text"
+        },
+        "blocked": {
+        "type": "boolean"
+        },
+        "@timestamp": {
+        "type": "date"
+        },
+        "time": {
+        "type": "date",
+        },
+    }
+  }
+    }))
     async with websockets.connect("wss://contentblocking.bitahoy.cloud/ws") as websocket:
         await websocket.send(json.dumps({"action": "auth", "token": token}))
         await websocket.recv()
@@ -23,12 +51,22 @@ async def poll_blocked_domains(token):
         while True:
             await websocket.send(json.dumps({"action": "get_data_detailed", "id": lastid}))
             message = json.loads(await websocket.recv())
+            print(f"pulled {len(message['detailed'])} new records")
+            items = []
             for item in message["detailed"]:
                 lastid = max(lastid, item["id"])
                 index = item["id"]
-                item["@timestamp"] = item["time"]
+                item["@timestamp"] = item["time"] * 1000
+                item["_id"] = index
+
                 del item["id"]
-                message = await conn.add_document('contentblocking', item, index)
+                items.append(item)
+                if len(items) >= 100:
+                    resp = await conn.add_documents('contentblocking', items)
+                    print(resp)
+                    items = []
+            if len(items) > 0:
+                message = await conn.add_documents('contentblocking', items)
                 print(message)
             await asyncio.sleep(10)
 
--- soarflow/src/opensearch.py
+++ soarflow/src/opensearch.py
@@ -11,8 +11,23 @@ class OpenSearchConnection:
             async with session.get(self.base_url) as resp:
                 return await resp.json()
 
-    async def create_index(self, index_name, settings={}):
+    async def get_index_settings(self, index_name):
         async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            async with session.get(self.base_url + "/" + index_name + "/_settings") as resp:
+                return await resp.json()
+
+    async def get_index_mappings(self, index_name):
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            async with session.get(self.base_url + "/" + index_name + "/_mapping") as resp:
+                return await resp.json()
+
+    async def create_index(self, index_name, settings={}, override=False):
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            mappings = await self.get_index_mappings(index_name)
+            if not override and index_name in mappings:
+                return mappings[index_name]
+            elif override:
+                await self.delete_index(index_name)
             async with session.put(self.base_url + "/" + index_name, json=settings) as resp:
                 return await resp.json()
 
@@ -29,3 +44,18 @@ class OpenSearchConnection:
             else:
                 async with session.post(self.base_url + "/" + index_name + "/_doc/" + str(index), json=document) as resp:
                     return await resp.json()
+
+    async def add_documents(self, index_name, documents):
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+            data = ""
+            for document in documents:
+                header = {"index": {}}
+                if "_id" in document:
+                    header["index"]["_id"] = document["_id"]
+                    del document["_id"]
+                data += json.dumps(header) + "\n"
+                data += json.dumps(document) + "\n"
+                
+
+            async with session.post(self.base_url + "/" + index_name + "/_bulk", data=data, headers={'content-type':'application/json', 'charset':'UTF-8'}) as resp:
+                return await resp.json()
